65e719a2cd7b28df833502c33f7f820e61c43b18
João Pavão <jppavao@co.sapo.pt>
2007-07-17 16:17:10 +0000

    Added a script and other support files that may be used to automate the generation of nightly builds. Refs #44.
    git-svn-id: svn://svn.softwarelivre.sapo.pt/sapo_msg_mac/trunk@57 30d2cb3b-9f40-45d5-ba16-273292637043

---
+++ nightly_build/make-nightly-build.sh
@@ -0,0 +1,158 @@
+#!/bin/sh
+
+SVN_TRUNK_URL='svn://svn.softwarelivre.sapo.pt/sapo_msg_mac/trunk'
+
+SRC_DIR='leapfrog-nightly-trunk'
+PROJ_SUBDIR='lilypad'
+PRODUCTS_SUBDIR='SAPO_Messenger'
+
+BUILDS_DIR='nightly_builds_server_mirror'
+APPCAST_FEED_FILENAME='appcast_feed.xml'
+
+APPCAST_FEED_SNIPPETS_DIR='appcast_feed_snippets'
+LOGS_XSLT_STYLESHEET='svnlog2html.xsl'
+
+MAX_NR_BUILDS_TO_KEEP=5
+
+NIGHTLIES_SERVER_DIR='nightly_builds'
+URL_PREFIX="http://messenger.sapo.pt/software_update/mac/${NIGHTLIES_SERVER_DIR}"
+
+
+####################################
+#  . Useful functions
+
+function list_sorted_available_build_nrs {
+	ls "${BUILDS_DIR}"/*.zip | sed 's/.*_\([0-9]*\).zip/\1/' | sort -rn
+}
+
+
+####################################
+# 1. Make a new build
+
+echo
+echo "==================================================================="
+date
+echo
+
+set -x
+
+export PATH=$PATH:/usr/local/bin
+
+
+if [ ! -d "$SRC_DIR" ]; then
+    svn co "$SVN_TRUNK_URL" "$SRC_DIR"
+fi
+
+REVISION=`svnversion "$SRC_DIR"`
+BUILD_NR=$(( $REVISION + 500 ))
+
+PREV_BUILD_NR=`list_sorted_available_build_nrs | head -1`
+PREV_REVISION=$(( ${PREV_BUILD_NR:=$(( $BUILD_NR - 1 ))} - 500 ))
+
+
+cd "$SRC_DIR" || exit 1
+
+# clean up
+make clean || rm -fR .moc .obj
+rm -fR Makefile xcode_conf.pri "$PRODUCTS_SUBDIR/Makefile" "$PRODUCTS_SUBDIR/SAPO_Messenger.app" "$PRODUCTS_SUBDIR/SAPO Messenger.app"
+
+
+svn update
+
+
+NEW_ARCHIVE_FILENAME="SAPO_Messenger-build_${BUILD_NR}.zip"
+NEW_ARCHIVE_PATHNAME="../${BUILDS_DIR}/${NEW_ARCHIVE_FILENAME}"
+
+if [ "$REVISION" -a ! -f "$NEW_ARCHIVE_PATHNAME" ]; then
+	
+	cd "$PROJ_SUBDIR"
+	xcodebuild	-project Lilypad.xcodeproj \
+				-target Leapfrog \
+				-configuration Release \
+				clean build
+	cd ..
+	
+	
+	if [ -d "$PRODUCTS_SUBDIR/SAPO Messenger.app" ]; then
+		mkdir -p "../$BUILDS_DIR"
+		
+		# Create a ZIP archive using ditto to preserve resource forks
+		ditto -V -c -k --keepParent "$PRODUCTS_SUBDIR/SAPO Messenger.app" "$NEW_ARCHIVE_PATHNAME"
+	fi
+else
+	# No new revision, just bail out.
+	exit 0
+fi
+
+cd ..
+
+
+####################################
+# 2. Generate the appcast description for this new build
+
+SNIPPET_XML_FILENAME="${BUILD_NR}_svnlog.xml"
+SNIPPET_HTML_FILENAME="${BUILD_NR}_description.html"
+
+mkdir -p "$APPCAST_FEED_SNIPPETS_DIR"
+cd "$APPCAST_FEED_SNIPPETS_DIR"
+
+svn log --xml -r $(( $PREV_REVISION + 1 )):$REVISION "../$SRC_DIR" > "$SNIPPET_XML_FILENAME"
+xsltproc "../$LOGS_XSLT_STYLESHEET" "$SNIPPET_XML_FILENAME" > "$SNIPPET_HTML_FILENAME"
+
+cd ..
+
+
+####################################
+# 3. Clean up old files
+
+for B in `list_sorted_available_build_nrs | tail +$(( $MAX_NR_BUILDS_TO_KEEP + 1 ))`; do
+	rm -f "${BUILDS_DIR}/SAPO_Messenger-build_${B}.zip"
+	rm -f "${APPCAST_FEED_SNIPPETS_DIR}/${B}_description.html"
+	rm -f "${APPCAST_FEED_SNIPPETS_DIR}/${B}_svnlog.xml"
+done
+
+
+####################################
+# 4. Re-generate the appcast feed
+
+cat > "${BUILDS_DIR}/${APPCAST_FEED_FILENAME}" <<ENDOFHEAD
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle"> 
+    <channel>
+        <title>SAPO Messenger Mac Appcast - Nightly Builds</title>
+        <link>http://messenger.sapo.pt</link>
+        <description>Most recent changes with links to updates.</description>
+        <language>en</language>
+		
+ENDOFHEAD
+
+for B in `list_sorted_available_build_nrs`; do
+	{	echo '<item>'
+		echo "    <title>Nightly Build ${B}</title>"
+        echo '    <description><![CDATA['
+		
+		cat "${APPCAST_FEED_SNIPPETS_DIR}/${B}_description.html"
+		
+		FILE_NAME="SAPO_Messenger-build_${B}.zip"
+		FILE_MOD_DATE=`stat -f '%Sm' "${BUILDS_DIR}/${FILE_NAME}"`
+		FILE_SIZE=`stat -f '%z' "${BUILDS_DIR}/${FILE_NAME}"`
+		FILE_URL="${URL_PREFIX}/${FILE_NAME}"
+		
+		echo '    ]]></description>'
+		echo "    <pubDate>${FILE_MOD_DATE}</pubDate>"
+		echo "    <enclosure url=\"${FILE_URL}\" sparkle:version=\"${B}\" sparkle:shortVersionString=\"1.0\" length=\"${FILE_SIZE}\" type=\"application/octet-stream\"/>"
+		echo '</item>'
+		echo
+				} >> "${BUILDS_DIR}/${APPCAST_FEED_FILENAME}"
+done
+
+cat >> "${BUILDS_DIR}/${APPCAST_FEED_FILENAME}" <<ENDOFTOE
+    </channel>
+</rss>
+ENDOFTOE
+
+
+####################################
+# 5. Rsync with the server
+
+./sync-nightly-builds-with-server.sh
---
+++ nightly_build/svnlog2html.xsl
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="html" media-type="html" indent="yes" />
+
+<xsl:template match="/">
+    <p>What's new:
+        <ul>
+            <xsl:for-each select="log/logentry">
+                <li><b>Rev. <xsl:value-of select="@revision" />:</b> <xsl:value-of select="msg" /></li>
+            </xsl:for-each>
+        </ul>
+    </p>
+</xsl:template>
+
+</xsl:stylesheet>
